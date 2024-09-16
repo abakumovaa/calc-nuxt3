2b712fcbc3d56459df16c401b45d05d55f1e26f8
Anastasia Abakumova <anastasia.abakumovaa@gmail.com>
2024-09-16 15:19:39 +0300

    написала верстку и стили для калькулятора

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        <h1>Hello world!</h1>
+    <div class="p-3" style="max-width: 400px; margin: 50px auto; background: #002b72">
+        <div class="w-full rounded m-1 p-3 text-end lead font-weight-bolder text-white bg-vue-light">
+            {{ calculatorValue || 0 }}
+        </div>
+    
+        <div class="row no-gutters">
+                <div class="col-3" v-for="n in calculatorElements"
+            :key="n">
+            <div class="lead text-white text-center m-1 py-2 bg-vue-light rounded hover-class"
+            :class="{'bg-vue-special-btn': ['MC', 'MR', 'M-', 'M+',
+                                      'MU', 'GT', 'CE', '√',
+                                      '±', '%', '/', '-',
+                                      '+', '*', '.', '='].includes(n)}" 
+            @click="action(n)"
+            >
+                {{ n }}
+            </div>
+        </div>
+        </div>
     </div>
     </template>
     
@@ -11,11 +28,35 @@
         props: {
             msg: String
         },
+        data() {
+        return {
+            calculatorValue: '',
+            calculatorElements: [
+            "MC", "MR", "M-", "M+",
+            "MU", "GT", "CE", "√",
+            7, 8, 9, "±", 4, 5, 6,
+            "%", 1, 2, 3, "/", 0, "*",
+            "+", "-", ".", "=",
+            ],
+            operator: null,
+            previousCalculatorValue: '',
+        }
+    }
     }
     
     </script>
     
     <style scoped>
 
+    .bg-vue-light {
+        background: #0bbbef;
+    }
+    .bg-vue-special-btn {
+        background-color: #1f72b8; 
+    }
+    .hover-class:hover {
+        cursor: pointer;
+        background-color: #003a97;
+    }
     
     </style>
